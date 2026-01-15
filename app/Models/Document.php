3d07834109b8d9f0566c325c9820bd1e54ea6b0d
talyx <ngwemoa@gmail.com>
2026-01-15 22:53:35 +0300

    fix: add fillable properties to Document model to allow mass assignment

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Document extends Model
 {
+    protected $fillable = ["customer_id", "file_type", "file_path"];
+
     public function customer()
     {
         return $this->belongsTo(Customer::class);
